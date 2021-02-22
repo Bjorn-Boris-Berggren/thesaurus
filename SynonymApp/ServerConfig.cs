eb2b6005f7f7e7119a546efa5836ad29f291c5bc
Björn Boris Berggren <info@bjornborisberggren.com>
2021-02-23 00:25:10 +0100

    Update ServerConfig.cs
    Missing word in the comment

--- SynonymApp/ServerConfig.cs
+++ SynonymApp/ServerConfig.cs
@@ -3,14 +3,14 @@
 namespace additude.thesaurus
 {
     /// <summary>
-    /// Here you configure the database used by Thesaurus
+    /// Here you can configure the database used by Thesaurus
     /// </summary>
     public abstract class ServerConfig
     {
-        public static readonly string Server = "[Server]";
-        public static readonly string Database = "[Database]";
-        public static readonly string User = "[User]";
-        public static readonly string Password = "[Password]";
+        public static readonly string Server = "<Server>";
+        public static readonly string Database = "<Database>";
+        public static readonly string User = "<User>";
+        public static readonly string Password = "<Password>";
 
     }
 }
